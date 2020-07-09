2564f71e37654c81e58cb3238358d4588e87ff29
Nicolavickh <nicolavickh@gmail.com>
2020-07-09 14:56:53 +0700

    API

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime? DateAdded { get; set; }
+
+        [Range(0, 20)]
+        public int Stock { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
